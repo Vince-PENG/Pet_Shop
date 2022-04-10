34d89bb5ebc523ab62f77e673ba419ae3579a094
彭来仕 <laishi.peng@gingkoo.com>
2022-04-10 12:40:46 +0800

    完善细节

--- api/martDetail.js
+++ api/martDetail.js
@@ -5,7 +5,7 @@ export default {
 	"itemNo": "5875254410113816001",
 	"storeId": 9203,
 	"barcode": "WD015875254410118605",
-	"name": "芝芝果茶系列 喜茶xCONTIGO联名芝芝/波波吸管杯",
+	"name": "三角巾围脖",
 	"categoryId": 5008066737,
 	"labelId": null,
 	"labelName": null,
@@ -13,7 +13,7 @@ export default {
 	"categoryDTO": {
 		"id": 5008066737,
 		"parentId": null,
-		"category": "杯类产品",
+		"category": "周边产品",
 		"categoryImgUrl": "https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/fe5bafefa36c4c07b1b4e8c8bf4cb9f8z",
 		"status": 1,
 		"seq": 28,
@@ -26,17 +26,17 @@ export default {
 		"parentCategory": null,
 		"outCategoryIdList": null
 	},
-	"salePrice": 14800,
-	"labelPrice": 14800,
+	"salePrice": 4800,
+	"labelPrice": 4800,
 	"memberPrice": null,
 	"itemSalesVolume": 5735,
 	"virtualSalesVolume": 0,
 	"isShelf": 1,
 	"itemStock": 626,
 	"itemWithholdStock": 0,
-	"describe": "<p><img src=\"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/636beabc152f42b4a53b03e965a6256a.png\" style=\"max-width: 100%;\"><img src=\"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/a0859178aef54178a7931cca26364577.png\" style=\"max-width: 100%;\"><img src=\"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/70d45373965244d7aa69694cf80dcec2.png\" style=\"max-width: 100%;\"><img src=\"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/37556ad39fbd43aaa245088017c15d44.png\" style=\"max-width: 100%;\"><img src=\"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/bf8f0864d0854829aa82c2fd7178e2ef.png\" style=\"max-width: 100%;\"><img src=\"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/8b40a73f6a9042a385ab4c65e053802f.png\" style=\"max-width: 100%;\"><img src=\"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/50f7f75426b9412b95d8848fc4090069.png\" style=\"max-width: 100%;\"><img src=\"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/80a346fac54749a1acad9c8ec115b11f.png\" style=\"max-width: 100%;\"><img src=\"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/e2109b23aa354853a0faed67ad14e3ed.png\" style=\"max-width: 100%;\"><img src=\"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/21dccf357bc74cc0a0faecf39c13a421.png\" style=\"max-width: 100%;\"><img src=\"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/debb7001eaea4fe1aaa5012820d55abf.png\" style=\"max-width: 100%;\"><img src=\"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/c3b7d74a6a0545d6857e9ed1bc26de9a.png\" style=\"max-width: 100%;\"><img src=\"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/b5dfcec384c94d609572e88bf7fc0f54.png\" style=\"max-width: 100%;\"><img src=\"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/f1ca48c88dc9438abfefa9ca57c43dca.png\" style=\"max-width: 100%;\"></p>",
+	"describe": "<p><img src=\"/static/images/mart/detail/baihuo_d_1.jpg\" style=\"max-width: 100%;\"><img src=\"/static/images/mart/detail/baihuo_d_2.jpg\" style=\"max-width: 100%;\"><img src=\"/static/images/mart/detail/baihuo_d_3.jpg\" style=\"max-width: 100%;\"><img src=\"/static/images/mart/detail/baihuo_d_4.jpg\" style=\"max-width: 100%;\"><img src=\"/static/images/mart/detail/baihuo_d_5.jpg\" style=\"max-width: 100%;\"><img src=\"/static/images/mart/detail/baihuo_d_6.jpg\" style=\"max-width: 100%;\"></p>",
 	"status": 1,
-	"thumbnail": "https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/f3a26c6ce9594b1e97324c82798f6c71.png",
+	"thumbnail": "",
 	"tag1": 0,
 	"unit": "个",
 	"paySubtractStock": false,
@@ -61,17 +61,15 @@ export default {
 	"width": null,
 	"height": null,
 	"volume": null,
-	"subName": "仅波波吸管杯&芝芝吸管杯有货",
+	"subName": "有货",
 	"peopleLimitAmount": null,
-	"placeOfDispatch": "广东广州",
+	"placeOfDispatch": "上海市",
 	"shelfTime": "2020-05-23T02:04:50.000+0000",
 	"preSell": false,
 	"materialUrls": [
-		"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/f3a26c6ce9594b1e97324c82798f6c71.png",
-		"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/524757c5ea534781b18fb882cdb4f650.png",
-		"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/f5bc97d7e70e4c5e9d516dde70dc9701.png",
-		"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/6300ca05a21e4283869b090a0e5ffec7.png",
-		"https://prod-mall-cos-1252929494.cos.ap-guangzhou.myqcloud.com/2307edb41ce8451bb5fbb20c4dad05a1.png"
+		"/static/images/mart/detail/baihuo_d_1.jpg",
+		"/static/images/mart/detail/baihuo_d_2.jpg",
+		"/static/images/mart/detail/baihuo_d_4.jpg",
 	],
 	"skuTreeList": [{
 		"keyId": 116,
@@ -79,121 +77,11 @@ export default {
 		"keyName": "款式",
 		"showImage": 0,
 		"treeValList": [{
-			"valId": 1700,
-			"attrValSort": 0,
-			"valName": "莓莓吸管杯",
-			"imageId": null,
-			"imageUrl": null
-		}, {
-			"valId": 1701,
-			"attrValSort": 1,
-			"valName": "桃桃吸管杯",
-			"imageId": null,
-			"imageUrl": null
-		}, {
-			"valId": 1707,
-			"attrValSort": 2,
-			"valName": "芝芝吸管杯",
-			"imageId": null,
-			"imageUrl": null
-		}, {
-			"valId": 1708,
-			"attrValSort": 3,
-			"valName": "波波吸管杯",
-			"imageId": null,
-			"imageUrl": null
-		}, {
 			"valId": 1709,
 			"attrValSort": 4,
-			"valName": "葡萄吸管杯",
+			"valName": "围脖",
 			"imageId": null,
 			"imageUrl": null
 		}]
-	}],
-	"skuInfoList": [{
-		"skuId": 12410,
-		"skuBarcode": "50100830",
-		"salePrice": 14800,
-		"labelPrice": 14800,
-		"memberPrice": null,
-		"stock": 0,
-		"withholdStock": 0,
-		"skuInfoNames": [{
-			"keyId": 116,
-			"attrKeySort": 0,
-			"keyName": "款式",
-			"valId": 1700,
-			"attrValSort": null,
-			"valName": "莓莓吸管杯"
-		}],
-		"itemNo": "5875254410113816001"
-	}, {
-		"skuId": 12411,
-		"skuBarcode": "50100832",
-		"salePrice": 14800,
-		"labelPrice": 14800,
-		"memberPrice": null,
-		"stock": 0,
-		"withholdStock": 0,
-		"skuInfoNames": [{
-			"keyId": 116,
-			"attrKeySort": 0,
-			"keyName": "款式",
-			"valId": 1701,
-			"attrValSort": null,
-			"valName": "桃桃吸管杯"
-		}],
-		"itemNo": "5875254410113816001"
-	}, {
-		"skuId": 12416,
-		"skuBarcode": "50100833",
-		"salePrice": 14800,
-		"labelPrice": 14800,
-		"memberPrice": null,
-		"stock": 222,
-		"withholdStock": 0,
-		"skuInfoNames": [{
-			"keyId": 116,
-			"attrKeySort": 0,
-			"keyName": "款式",
-			"valId": 1707,
-			"attrValSort": null,
-			"valName": "芝芝吸管杯"
-		}],
-		"itemNo": "5875254410113816001"
-	}, {
-		"skuId": 12417,
-		"skuBarcode": "50100831",
-		"salePrice": 14800,
-		"labelPrice": 14800,
-		"memberPrice": null,
-		"stock": 404,
-		"withholdStock": 0,
-		"skuInfoNames": [{
-			"keyId": 116,
-			"attrKeySort": 0,
-			"keyName": "款式",
-			"valId": 1708,
-			"attrValSort": null,
-			"valName": "波波吸管杯"
-		}],
-		"itemNo": "5875254410113816001"
-	}, {
-		"skuId": 12418,
-		"skuBarcode": "50100829",
-		"salePrice": 14800,
-		"labelPrice": 14800,
-		"memberPrice": null,
-		"stock": 0,
-		"withholdStock": 0,
-		"skuInfoNames": [{
-			"keyId": 116,
-			"attrKeySort": 0,
-			"keyName": "款式",
-			"valId": 1709,
-			"attrValSort": null,
-			"valName": "葡萄吸管杯"
-		}],
-		"itemNo": "5875254410113816001"
 	}]
 }
